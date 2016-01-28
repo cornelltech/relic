691cde3653136cc9adf2df5a1a24464811d3f2c4
James <james.kizer@gmail.com>
2016-01-28 14:17:41 -0500

    Added docker preset.

---
+++ preset/docker-x64.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# These settings were selected to get very good performance on a
+# 64-bit Intel architecture using the BN pairing-based curves for the
+# Pythia protocol. Updated for docker depoloyment.
+
+cmake -DALLOC=AUTO -DARITH=easy \
+    -DFP_QNRES=on -DFP_PRIME=254 -DCHECK=off \
+    -DCOMP="-O3 -funroll-loops -fomit-frame-pointer -march=native -mtune=native" \
+    -DFP_METHD="BASIC;COMBA;COMBA;MONTY;LOWER;SLIDE" \
+    -DFPX_METHD="INTEG;INTEG;LAZYR" \
+    -DPP_METHD="LAZYR;OATEP" $1
